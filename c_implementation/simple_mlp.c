b339225a031d5055c0c5bcd1e16fc35559d6c691
Milos Arbanas <milostravian2@gmail.com>
2019-05-12 17:19:42 +0200

    OpenMP implementation now works as generic for custom layer numbers and sizes. Should try to retrain and test another network.

--- c_implementation/simple_mlp.c
+++ c_implementation/simple_mlp.c
@@ -7,57 +7,21 @@
 #include "load_parameters.h"
 #include "read_image.h"
 
-#define PIC_SIZE 28
-#define INPUT_SIZE 28*28
-#define CLASS_NUM 10
-
-#define n_input  784
-#define n_layer1  2048
-#define n_layer2  1024 
-#define n_layer3  512
-#define n_output  10
-const int LAYER_SIZE[5] = {n_input, n_layer1, n_layer2, n_layer3, n_output};
-
 static void softmax(float *input, size_t input_len) {
     float * Z = input;
     int K = input_len;
-
     float sum = 0;
-    for(int j = 0; j < K; j++)
-    {
+    for(int j = 0; j < K; j++){
         sum += exp(Z[j]);
     }
-
-    for(int i = 0; i < K; i++)
-    {
+    for(int i = 0; i < K; i++){
         input[i] = exp(input[i])/sum;
     } 
-
 }
 
 static void relu(float *input, int input_len){
-    for(int i = 0; i < input_len; i++)
-    {
+    for(int i = 0; i < input_len; i++){
         input[i] = fmax((float)0, input[i]);
-    }
-}
-
-
-//  Placeholders for calculations
-float input[INPUT_SIZE];
-float L1[n_layer1];
-float L2[n_layer2];
-float L3[n_layer3];
-float Loutput[n_output];
-
-void flatten_image(float ** image)
-{
-    for(size_t i = 0; i < PIC_SIZE; i++)
-    {
-        for(size_t j = 0; j < PIC_SIZE; j++)
-        {
-            input[i*PIC_SIZE+j] = image[i][j];
-        }
     }
 }
 
@@ -90,34 +54,23 @@ void calculate_layer(int layer_num, int*layer_size, float* input_matrix, float *
     
 }
 
-void load_input(float* image)
-{
-	for (int i = 0; i < n_input; i++)
-	{
-		input[i] = image[i];
-	}
-}
+void forward_propagation(int layer_num, int *layer_size,float ***weights, 
+                            float **biases, float *input, float **output){
+    
+    float **L = (float**) malloc(layer_num*sizeof(float*));
+    for (size_t i = 0; i < layer_num; i++)
+    {
+        L[i] = (float*) calloc(layer_size[i+1], sizeof(float));
+        printf("Allocating %d floats for layer %d\n", layer_size[i+1], i);
+    }
+    
+    calculate_layer(1, layer_size, input, weights[0], biases[0], L[0], 1);    
+    calculate_layer(2, layer_size, L[0], weights[1], biases[1], L[1], 1);
+    calculate_layer(3, layer_size, L[1], weights[2], biases[2], L[2], 1);
+    calculate_layer(4, layer_size, L[2], weights[3], biases[3], L[3], 2);
 
-void compare_1d_array(float *x, float *y, int length)
-{
-	float max_error = 0;
-    int max_index = -1;
-	for (int i = 0; i < length; i++){
-		if (fabs((x[i]-y[i])) > max_error)
-		{
-			max_error = fabs((x[i] - y[i]));
-            max_index = i;
-		}
-	}
-	printf("Max error = %f on index %d\n", max_error, max_index);
-}
+    (*output) = L[3];
 
-void forward_propagation(int *layer_size,float ***weights, 
-                            float **biases, float *input){
-    calculate_layer(1, layer_size, input, (float *)weights[0], biases[0], L1, 1);    
-    calculate_layer(2, layer_size, L1, (float *)weights[1], biases[1], L2, 1);
-    calculate_layer(3, layer_size, L2, (float *)weights[2], biases[2], L3, 1);
-    calculate_layer(4, layer_size, L3, (float *)weights[3], biases[3], Loutput, 2);
 }
 
 void main(int argc, char **argv)
@@ -135,8 +88,9 @@ void main(int argc, char **argv)
 
     struct timeval  tv1, tv2;
     gettimeofday(&tv1, NULL);
-   
-    forward_propagation(layer_sizes, weights, biases, flatten_image);
+    float *output;
+    forward_propagation(layer_num, layer_sizes, 
+                weights, biases, flatten_image, &output);
 
     gettimeofday(&tv2, NULL);
 
@@ -145,11 +99,10 @@ void main(int argc, char **argv)
          (float) 1000000*(tv2.tv_sec - tv1.tv_sec));
 
     printf("Izracunat izlaz: ");
-    for(size_t i = 0; i < CLASS_NUM; i++)
+    for(size_t i = 0; i < layer_sizes[layer_num]; i++)
     {
-        printf("\t%.2f", Loutput[i]);
+        printf("\t%.2f", output[i]);
     }
     printf("\n");
-    
-	
+    	
 }
